5ea9426c6635fdcf67fd619f7ce7671a4f1bf995
bbeyer <beth.beyer1@gmail.com>
2019-10-23 12:18:50 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,14 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+
+export_file_url = 'https://www.dropbox.com/s/ayhft8xfu8el9sx/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+#classes = ['black', 'grizzly', 'teddys']
+
+classes + ['brain', 'seafan']
 path = Path(__file__).parent
 
 app = Starlette()
